60ccbec64ef185b9d0935349b53f57e5a27514a0
Aoneshima <138617018+Aoneshima@users.noreply.github.com>
2024-09-15 15:45:03 +0300

    Delete проект по DOM добавление объектов directory

--- проект по DOM добавление объектов/scripts/recalculate.js
+++
@@ -1,9 +0,0 @@
-// let itemNumber;
-function recalculateNumber(){
-    for(let i = 0; i < items.children.length; i++){
-        let itemChildren = items.children[i];
-        itemNumber = itemChildren.querySelector('.item-number');
-
-        itemNumber.textContent = i + 1;
-    }
-}
